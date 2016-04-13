3a0a0e462fa2ffe0b06574a7a4a5056dae658edf
Rondinelli Morais <rondinele.morais@portoseguro.com.br>
2016-04-13 11:45:40 -0300

    create methods to parse UIView on Image and UIView to Image base on frame

--- RMExtensionKit/UIView.swift
+++ RMExtensionKit/UIView.swift
@@ -21,4 +21,23 @@ extension UIView {
         self.layer.borderColor = borderColor.CGColor
         self.layer.borderWidth = borderWidth
     }
+    
+    public func toImage() -> UIImage? {
+        
+        UIGraphicsBeginImageContextWithOptions(self.frame.size, self.opaque, UIScreen.mainScreen().scale)
+        self.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    public func toImage(frame: CGRect) -> UIImage? {
+        
+        UIGraphicsBeginImageContextWithOptions(self.frame.size, false, UIScreen.mainScreen().scale)
+        CGContextClipToRect(UIGraphicsGetCurrentContext(), frame)
+        self.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
 }
